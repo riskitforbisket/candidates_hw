d5d062be891b0801d124a14e2bfbdc164d303765
Daniel Irizarry <danielssimplerobotics@gmail.com>
2017-05-11 20:10:12 -0500

    Verification of cv2 videocapture working while displaying frames in loop.

--- calcWeldWidth.py
+++ calcWeldWidth.py
@@ -1,6 +1,16 @@
 import numpy as np
 import cv2
 
+#for easy frame visualization
+import matplotlib.pyplot as plt
+
+#im: numpy array, raw video frame
+#prepIm: numpy array, prepared image ready for bead measurement
+def prepareImage(im):
+
+    return prepIm
+
+
 #im: numpy array, frame from weld video
 #outputs: weldWidth, approximate width of weld bead in pixels
 def measureWeldBead(im):
@@ -10,4 +20,16 @@ def measureWeldBead(im):
 
 if __name__ == "__main__":
     print "Daniel Irizarry - The Best!"
+
+    #open video file and pull the first frame
+    video = cv2.VideoCapture("./videos/weld.mp4")
+    ok, frame = video.read()
+
+    #while we still get a valid frame
+    while ok:
+        ok, frame = video.read()
+        print frame.shape
+        plt.imshow(frame)
+        plt.show()
+
     widthInPixels = measureWeldBead(np.arange(1))
